5a0fad8a6237346a71c2b937bcacb433bfe1b49f
Axwabo <axwabo@gmail.com>
2026-07-23 16:29:02 +0200

    2 tests

--- PipeCopyTest/Program.cs
+++ PipeCopyTest/Program.cs
@@ -3,24 +3,30 @@
 await using var outStream = Console.OpenStandardOutput();
 await using var outWriter = new StreamWriter(outStream);
 outWriter.AutoFlush = true;
-
-var pipe1 = new Pipe();
-var pipe2 = new Pipe();
-
-await Task.WhenAll(
-    Task.Run(() => pipe1.Reader.CopyToAsync(outStream)),
-    Task.Run(async () =>
-    {
-        await using var writer = pipe1.Writer.AsStream();
-        await pipe2.Reader.CopyToAsync(writer);
-    }),
-    WriteAsync()
-);
-
+await Test();
+await Test();
 return;
 
-async Task WriteAsync()
+async Task Test()
 {
-    await using var writer = new StreamWriter(pipe2.Writer.AsStream());
-    await writer.WriteLineAsync("hewooooo");
+    var pipe1 = new Pipe();
+    var pipe2 = new Pipe();
+
+    await Task.WhenAll(
+        Task.Run(() => pipe1.Reader.CopyToAsync(outStream)),
+        Task.Run(async () =>
+        {
+            await using var writer = pipe1.Writer.AsStream();
+            await pipe2.Reader.CopyToAsync(writer);
+        }),
+        WriteAsync()
+    );
+
+    return;
+
+    async Task WriteAsync()
+    {
+        await using var writer = new StreamWriter(pipe2.Writer.AsStream());
+        await writer.WriteLineAsync("hewooooo");
+    }
 }
