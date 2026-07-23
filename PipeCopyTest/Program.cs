cc5881551e6ab754714f70a1c5f780dc63eb24ab
Axwabo <axwabo@gmail.com>
2026-07-23 16:29:06 +0200

    Revert "2 tests"
    This reverts commit 5a0fad8a6237346a71c2b937bcacb433bfe1b49f.

--- PipeCopyTest/Program.cs
+++ PipeCopyTest/Program.cs
@@ -3,30 +3,24 @@
 await using var outStream = Console.OpenStandardOutput();
 await using var outWriter = new StreamWriter(outStream);
 outWriter.AutoFlush = true;
-await Test();
-await Test();
+
+var pipe1 = new Pipe();
+var pipe2 = new Pipe();
+
+await Task.WhenAll(
+    Task.Run(() => pipe1.Reader.CopyToAsync(outStream)),
+    Task.Run(async () =>
+    {
+        await using var writer = pipe1.Writer.AsStream();
+        await pipe2.Reader.CopyToAsync(writer);
+    }),
+    WriteAsync()
+);
+
 return;
 
-async Task Test()
+async Task WriteAsync()
 {
-    var pipe1 = new Pipe();
-    var pipe2 = new Pipe();
-
-    await Task.WhenAll(
-        Task.Run(() => pipe1.Reader.CopyToAsync(outStream)),
-        Task.Run(async () =>
-        {
-            await using var writer = pipe1.Writer.AsStream();
-            await pipe2.Reader.CopyToAsync(writer);
-        }),
-        WriteAsync()
-    );
-
-    return;
-
-    async Task WriteAsync()
-    {
-        await using var writer = new StreamWriter(pipe2.Writer.AsStream());
-        await writer.WriteLineAsync("hewooooo");
-    }
+    await using var writer = new StreamWriter(pipe2.Writer.AsStream());
+    await writer.WriteLineAsync("hewooooo");
 }
