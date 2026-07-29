98e13d17f1f1b26b39977ca9c0a8d64cb59e56aa
Axwabo <axwabo@gmail.com>
2026-07-29 21:52:23 +0200

    interactive shelll by default

--- BaSL/Console.cs
+++ BaSL/Console.cs
@@ -42,7 +42,8 @@ public sealed class Console
     public async Task<int> StartAsync()
     {
         await using var context = _context;
-        _shell = new BaShell(context, new StandaloneStatement(Path.Binaries / "echo", new[] {"hello world"}) /* | Path.Binaries / "cat"*/);
+        var statement = new StandaloneStatement(Path.Binaries / "echo", new[] {"hello world"}) /* | Path.Binaries / "cat"*/;
+        _shell = new BaShell(context /*, statement*/);
         return await _shell.ExecuteAsync(CancellationToken.None);
     }
 
