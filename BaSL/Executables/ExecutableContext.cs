0d35da161e6785e1114606b5b4198db7acb7554a
Axwabo <axwabo@gmail.com>
2026-07-29 21:07:51 +0200

    Revert "attempt derived classes"
    This reverts commit b8cd6d33ffae3ff8c79bcf16612c4af8fc0268a7.

--- BaSL/Executables/ExecutableContext.cs
+++ BaSL/Executables/ExecutableContext.cs
@@ -7,14 +7,15 @@ using Directory = BaSL.FileSystems.Directory;
 
 namespace BaSL.Executables;
 
-public abstract class ExecutableContext
+public sealed class ExecutableContext
 {
 
     internal static ExecutableContext Root(Console console, FileSystem fileSystem, ReadOnlyMemory<string> args, StreamWriter standardOutput, StreamWriter standardError)
-    {
-        var inputPipe = new PipeWrapper();
-        return new RootExecutableContext(console, fileSystem, console.CurrentDirectory, args, inputPipe, standardOutput, standardError);
-    }
+        => new(console, fileSystem, console.CurrentDirectory, args)
+        {
+            SourceOutput = standardOutput,
+            SourceError = standardError
+        };
 
     internal static ExecutableContext Piped(ExecutableContext source, Console console, FileSystem fileSystem, ReadOnlyMemory<string> args) => new(console, fileSystem, console.CurrentDirectory, args)
     {
@@ -46,39 +47,72 @@ public abstract class ExecutableContext
         }
     }
 
-    private protected ExecutableContext(
+    private bool _disposed;
+
+    private ExecutableContext(
         Console console,
         FileSystem fileSystem,
         Directory workingDirectory,
-        ReadOnlyMemory<string> args,
-        StreamReader sourceInput,
-        StreamWriter sourceOutput,
-        StreamWriter sourceError
+        ReadOnlyMemory<string> args
     )
     {
+        StandardInput = new PipeWrapper();
+        StandardOutput = new PipeWrapper();
+        StandardError = new PipeWrapper();
         Console = console;
         FileSystem = fileSystem;
         WorkingDirectory = workingDirectory;
         Args = args;
-        SourceInput = sourceInput;
-        SourceOutput = sourceOutput;
-        SourceError = sourceError;
+        SourceInput = StandardInput.Reader;
+        SourceOutput = StandardOutput.Writer;
+        SourceError = StandardError.Writer;
+        DestinationInput = StandardInput.Writer;
+        DestinationOutput = StandardOutput.Reader;
+        DestinationError = StandardError.Reader;
     }
 
+    private ExecutableContext? Parent { get; init; }
+    internal PipeWrapper? StandardInput { get; init; }
+    internal PipeWrapper? StandardOutput { get; init; }
+    internal PipeWrapper? StandardError { get; init; }
     internal Console Console { get; }
     internal FileSystem FileSystem { get; }
     internal Directory WorkingDirectory { get; }
     internal ReadOnlyMemory<string> Args { get; }
-    internal StreamReader SourceInput { get; }
-    internal StreamWriter SourceOutput { get; }
-    internal StreamWriter SourceError { get; }
-    internal abstract StreamWriter DestinationInput { get; }
-    internal abstract StreamReader DestinationOutput { get; }
-    internal abstract StreamReader DestinationError { get; }
-    internal abstract bool IsRoot { get; }
+    internal StreamReader SourceInput { get; private init; }
+    internal StreamWriter SourceOutput { get; private init; }
+    internal StreamWriter SourceError { get; private init; }
+    internal StreamWriter DestinationInput { get; }
+    internal StreamReader DestinationOutput { get; }
+    internal StreamReader DestinationError { get; }
+    private bool DisposeOutput { get; init; }
+    internal bool IsRoot => Parent == null;
 
-    internal abstract Task CopyAsync();
+    internal async Task CopyAsync(bool copyStdin)
+    {
+        if (Parent == null)
+            return;
+        try
+        {
+            await Task.WhenAll(
+                copyStdin ? CopyAsync(Parent.SourceInput, DestinationInput, StandardInput) : Task.CompletedTask,
+                CopyAsync(DestinationOutput, Parent.SourceOutput, StandardOutput, DisposeOutput),
+                CopyAsync(DestinationError, Parent.SourceError, StandardError)
+            );
+        }
+        catch (OperationCanceledException) when (_disposed)
+        {
+        }
+    }
 
-    internal abstract ValueTask DisposeAsync();
+    internal async ValueTask DisposeAsync()
+    {
+        _disposed = true;
+        await StandardInput.DisposeAsync();
+        await StandardOutput.DisposeAsync();
+        await StandardError.DisposeAsync();
+        if (DisposeOutput)
+            await SourceOutput.DisposeAsync();
+    }
 
 }
