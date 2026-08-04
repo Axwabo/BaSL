82aa1e892f3db8189b41f682a125e6d0541e033c
Axwabo <axwabo@gmail.com>
2026-08-04 18:32:52 +0200

    experiment

---
+++ Experiments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO.Pipelines;
+
+var pipe = new Pipe();
+await using (var writer = new StreamWriter(pipe.Writer.AsStream()))
+{
+    writer.AutoFlush = true;
+    await writer.WriteLineAsync("Hello world!");
+    await pipe.Writer.CompleteAsync();
+}
+
+using var reader = new StreamReader(pipe.Reader.AsStream());
+Console.WriteLine("First:");
+Console.WriteLine(await reader.ReadLineAsync());
+Console.WriteLine("Second:");
+var line = await reader.ReadLineAsync();
+Console.WriteLine(line ?? "Ended");
