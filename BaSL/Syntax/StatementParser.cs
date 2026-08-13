2980d5edbc7c47ef492e99cc8aeea894c6c35e97
Axwabo <axwabo@gmail.com>
2026-08-13 21:14:48 +0200

    AddArg before == and !=

--- BaSL/Syntax/StatementParser.cs
+++ BaSL/Syntax/StatementParser.cs
@@ -132,11 +132,13 @@ internal static class StatementParser
                     break;
                 case (SyntaxType.Text, '=', '='):
                     i++;
+                    AddArg();
                     AddStatement(OperatorSegment.Eq);
                     outerSyntax = SyntaxType.Condition;
                     break;
                 case (SyntaxType.Text, '!', '='):
                     i++;
+                    AddArg();
                     AddStatement(OperatorSegment.NotEq);
                     outerSyntax = SyntaxType.Condition;
                     break;
