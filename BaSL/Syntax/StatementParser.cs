f86fbb7efc250dd65892d4cdfad25e75c0a24738
Axwabo <axwabo@gmail.com>
2026-08-22 16:36:57 +0200

    escape everything

--- BaSL/Syntax/StatementParser.cs
+++ BaSL/Syntax/StatementParser.cs
@@ -83,10 +83,7 @@ internal static class StatementParser
             char? next = i < s.Length - 1 ? s[i + 1] : null;
             switch (syntax, c, next)
             {
-                case (SyntaxType.Text or SyntaxType.QuotedString, '\\', '$' or '\\' or '\\'):
-                case (SyntaxType.Text, '\\', ' ' or '\'' or '\"'):
-                case (SyntaxType.VerbatimString, '\\', '\''):
-                case (SyntaxType.QuotedString, '\\', '"'):
+                case (not SyntaxType.VerbatimString, '\\', _):
                     i++;
                     argBuzilder.Append(next);
                     break;
