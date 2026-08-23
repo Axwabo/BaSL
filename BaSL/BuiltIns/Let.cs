7f60c2d53b2dfffedaabb3eba9db440c592d3954
Axwabo <axwabo@gmail.com>
2026-08-23 11:56:36 +0200

    implement ++ -- and shorthands

--- BaSL/BuiltIns/Let.cs
+++ BaSL/BuiltIns/Let.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using BaSL.Executables;
 using BaSL.Executables.Attributes;
 
 namespace BaSL.BuiltIns;
+
+using Result = Result<int, Error>;
 
 [Help("""
       Evaluates a simple integer arithmetic expression (constant or two operands), and sets a variable to that value.
@@ -17,9 +20,26 @@ namespace BaSL.BuiltIns;
 internal sealed partial class Let : BuiltInCommand
 {
 
-    private const string Operators = "+=*/%";
+    private const string Operators = "+-*/%";
 
     private static readonly Error DivideByZero = new DivideByZeroError();
+
+    private static Result Evaluate(int x, char @operator, int y)
+    {
+        Result result = @operator switch
+        {
+            '+' => x + y,
+            '-' => x - y,
+            '*' => x * y,
+            '/' when y == 0 => DivideByZero,
+            '/' => x / y,
+            '%' => x % y,
+            _ => 0
+        };
+        return result;
+    }
+
+    private string _arg = "";
 
     private int _lastResult;
 
@@ -31,60 +51,59 @@ internal sealed partial class Let : BuiltInCommand
     {
         foreach (var arg in Args)
         {
+            if (string.IsNullOrWhiteSpace(arg))
+                continue;
+            _arg = arg;
             var equals = arg.IndexOf('=');
             // TODO: proper variable name validation
-            if (equals < 1 || arg[0] is not (>= 'A' and <= 'Z' or >= 'a' and <= 'z'))
+            if (arg[0] is not (>= 'A' and <= 'Z' or >= 'a' and <= 'z'))
                 continue;
-            var name = arg[..equals];
-            switch (arg.AsSpan(equals + 1))
-            {
-                case [.. var name, '=']:
-                    if (int.TryParse(value, out))
-                        break;
-            }
+            if (equals < 1)
+                await SingleOperand(cancellationToken);
+            else
+                await Process(equals, cancellationToken);
         }
 
         return _lastResult == 0 ? 1 : 0;
     }
 
-    /*
-    protected  async void Process(string name, string value, CancellationToken cancellationToken)
+    private async Task Process(int equals, CancellationToken cancellationToken)
     {
-        var span = value.AsSpan();
-        var index = span.IndexOfAny(Operators);
-        if (index == -1)
+        var name = _arg.AsSpan(0, equals);
+        var value = _arg.AsSpan(equals + 1).Trim();
+        var index = value.IndexOfAny(Operators);
+        if (Operators.Contains(name[^1]))
         {
-            await SingleOperand(name, value, cancellationToken);
+            // TODO: evaluate left-hand expression
+            if (int.TryParse(value, out var selfY))
+                await EvaluateSelf(name[^1], selfY, name[..^1].ToString(), cancellationToken);
             return;
         }
 
-        if (index == span.Length - 1)
+        if (index == -1)
+        {
+            if (int.TryParse(value.Trim(), out var constant))
+                Store(name.ToString(), constant);
             return;
+        }
 
-        int.TryParse(span[..index].Trim(), out var x);
-        int.TryParse(span[(index + 1)..].Trim(), out var y);
-        await Evaluate(x, span[index], y, name, cancellationToken);
+        if (index == value.Length - 1)
+            return;
+        int.TryParse(value[..index].Trim(), out var x);
+        int.TryParse(value[(index + 1)..].Trim(), out var y);
+        await Evaluate(x, value[index], y, name.ToString(), cancellationToken);
     }
 
     private async Task Evaluate(int x, char @operator, int y, string name, CancellationToken cancellationToken)
     {
-        Result<int, Error> result = @operator switch
-        {
-            '+' => x + y,
-            '-' => x - y,
-            '*' => x * y,
-            '/' when y == 0 => DivideByZero,
-            '/' => x / y,
-            '%' => x % y,
-            _ => 0
-        };
+        var result = Evaluate(x, @operator, y);
         if (result.Success)
         {
             Store(name, result.Value);
             return;
         }
 
-        await StandardError.WriteAsync(Arg, cancellationToken);
+        await StandardError.WriteAsync(_arg, cancellationToken);
         await StandardError.WriteAsync(": ", cancellationToken);
         await StandardError.WriteLineAsync(result.Error, cancellationToken);
     }
@@ -95,25 +114,18 @@ internal sealed partial class Let : BuiltInCommand
         await Evaluate(value, @operator, y, name, cancellationToken);
     }
 
-    private async Task SingleOperand(string name, string value, CancellationToken cancellationToken)
+    private Task SingleOperand(CancellationToken cancellationToken) => _arg switch
     {
-        /*var x = name switch
-        {
-            ['+', '+'] => "",
-             _=>"a"
-        };#1#
-        if (value.EndsWith("++"))
-            await EvaluateSelf('+', 1, name, cancellationToken);
-        else if (int.TryParse(value.AsSpan().Trim(), out var single))
-            Store(name, single);
-    }
+        [.. var name, '+', '+'] => EvaluateSelf('+', 1, name, cancellationToken),
+        [.. var name, '-', '-'] => EvaluateSelf('-', 1, name, cancellationToken),
+        _ => Task.CompletedTask
+    };
 
     private void Store(string name, int result)
     {
         Local[name] = result.ToString();
         _lastResult = result;
     }
-    */
 
 }
 
