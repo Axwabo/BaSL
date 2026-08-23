5bf39a74d585f688ede1784d3125d5b656b0f3ca
Axwabo <axwabo@gmail.com>
2026-08-23 13:02:46 +0200

    also copy license

--- bundle.cs
+++ bundle.cs
@@ -2,6 +2,7 @@ using System.IO.Compression;
 
 const string licenses = "THIRD_PARTY_LICENSES";
 const string readme = "README.md";
+const string license = "LICENSE";
 const string terminal = "BaSL.Terminal";
 
 using var zipFile = File.Create(args[0]);
@@ -10,6 +11,7 @@ using var archive = new ZipArchive(zipFile, ZipArchiveMode.Create);
 var executable = OperatingSystem.IsWindows() ? $"{terminal}.exe" : terminal;
 archive.CreateEntryFromFile(executable, $"bin/{Path.GetFileName(executable)}");
 archive.CreateEntryFromFile($"../{readme}", readme);
+archive.CreateEntryFromFile($"../{license}", license);
 
 foreach (var file in Directory.EnumerateFiles($"../{licenses}"))
     archive.CreateEntryFromFile(file, $"{licenses}/{Path.GetFileName(file)}");
